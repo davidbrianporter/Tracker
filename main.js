5511b15362667b9e9d16ef167ff5bf15de711efb
David Porter <davidbrianporter@gmail.com>
2016-12-22 20:07:28 -0500

    created multiple windows

--- main.js
+++ main.js
@@ -7,4 +7,6 @@ var mainWindow = null;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({ width: 800, height: 600 })
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    smallWindow = new BrowserWindow({ width: 300, height: 200 })
+    smallWindow.loadURL('file://' + __dirname + '/ind.html');
 })
